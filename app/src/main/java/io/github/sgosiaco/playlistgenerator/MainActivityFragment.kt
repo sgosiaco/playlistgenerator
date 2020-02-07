76b4e37aa1e044205a84a9033d7cb083bd551440
sgosiaco <sgosiaco@me.com>
2020-02-06 21:41:01 -0800

    Added pull down to refresh list

--- app/src/main/java/io/github/sgosiaco/playlistgenerator/MainActivityFragment.kt
+++ app/src/main/java/io/github/sgosiaco/playlistgenerator/MainActivityFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -51,7 +52,22 @@ class MainActivityFragment : Fragment(), OnItemClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        swipeList.apply {
+            setOnRefreshListener {
+                updateList()
+                swipeList.isRefreshing = false
+            }
+            ContextCompat.getColor(context, android.R.color.holo_blue_light)
+            setColorSchemeColors(
+                ContextCompat.getColor(context, android.R.color.holo_blue_light),
+                ContextCompat.getColor(context, android.R.color.holo_green_light),
+                ContextCompat.getColor(context, android.R.color.holo_orange_light),
+                ContextCompat.getColor(context, android.R.color.holo_red_light)
+            )
+        }
+
         rv_song_list.apply {
+            setHasFixedSize(true)
             layoutManager = LinearLayoutManager(activity)
             adapter = SongAdapter(songs, this@MainActivityFragment)
             addItemDecoration(DividerItemDecoration(this.context, LinearLayoutManager.VERTICAL))
